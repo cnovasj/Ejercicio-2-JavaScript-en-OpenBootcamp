ee071560ba1ed49ebb32040b906119070a6b559d
Juanjo <104213446+cnovasj@users.noreply.github.com>
2022-07-01 14:34:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+const libro = {
+    titulo: "El extraño caso del Dr. Jekyll y Mr. Hyde",
+    autor: "Robert Louis Stevenson",
+    anyo: 1886,
+    url: "https://es.wikipedia.org/wiki/El_extra%C3%B1o_caso_del_doctor_Jekyll_y_el_se%C3%B1or_Hyde#:~:text=Editar-,El%20extra%C3%B1o%20caso%20del%20doctor%20Jekyll%20y%20el%20se%C3%B1or%20Hyde,de%20un%20abogado%2C%20Gabriel%20John"
+}
 const persona = {
     nombre: "Juanjo",
     edad: 24,
     desarrollador: false,
     nacimiento: new Date('1998-08-06T03:24:00'),
-    favlibro: {
-        titulo: "El extraño caso del Dr. Jekyll y Mr. Hyde",
-        autor: "Robert Louis Stevenson",
-        anyo: 1886,
-        url: "https://es.wikipedia.org/wiki/El_extra%C3%B1o_caso_del_doctor_Jekyll_y_el_se%C3%B1or_Hyde#:~:text=Editar-,El%20extra%C3%B1o%20caso%20del%20doctor%20Jekyll%20y%20el%20se%C3%B1or%20Hyde,de%20un%20abogado%2C%20Gabriel%20John"
-    }
+    favlibro: libro
 }
 
 console.log(persona);
